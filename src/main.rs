120dfbc0436ff137e75c080b06656c704db20135
liam-strand <Liam.Strand@tufts.edu>
2024-04-01 19:07:44 -0400

    basic uart connection

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,63 @@
-//! Blinks the LED on a Pico board
+//! # UART Example
 //!
-//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
+//! This application demonstrates how to use the UART Driver to talk to a serial
+//! connection.
+//!
+//! It may need to be adapted to your particular board layout and/or pin
+//! assignment.
+//!
+//! See the `Cargo.toml` file for Copyright and license details.
+
 #![no_std]
 #![no_main]
 
-use bsp::entry;
+// Ensure we halt the program on panic (if we don't mention this crate it won't
+// be linked)
+use panic_halt as _;
+
+// Alias for our HAL crate
+use rp_pico::hal;
+
+// A shorter alias for the Peripheral Access Crate, which provides low-level
+// register access
+use hal::pac;
+
 use defmt::*;
 use defmt_rtt as _;
-use embedded_hal::digital::v2::OutputPin;
-use panic_probe as _;
 
-// Provide an alias for our BSP so we can switch targets quickly.
-// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
-use rp_pico as bsp;
-// use sparkfun_pro_micro_rp2040 as bsp;
+// Some traits we need
+use hal::clocks::Clock;
+use hal::fugit::RateExtU32;
 
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac,
-    sio::Sio,
-    watchdog::Watchdog,
-};
+// UART related types
+use hal::uart::{DataBits, StopBits, UartConfig};
+use rp_pico::hal::uart::ReadErrorType;
 
-#[entry]
+use core::str;
+
+/// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
+/// if your board has a different frequency
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+/// Entry point to our bare-metal application.
+///
+/// The `#[rp2040_hal::entry]` macro ensures the Cortex-M start-up code calls this function
+/// as soon as all global variables and the spinlock are initialised.
+///
+/// The function configures the RP2040 peripherals, then writes to the UART in
+/// an infinite loop.
+#[hal::entry]
 fn main() -> ! {
-    info!("Program start");
+    // Grab our singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
     let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
 
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
+    // Set up the watchdog driver - needed by the clock setup code
+    let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -41,36 +65,106 @@ fn main() -> ! {
         &mut pac.RESETS,
         &mut watchdog,
     )
-    .ok()
+    .map_err(|_| "could not get it to work")
     .unwrap();
 
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+    let mut _delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
-    let pins = bsp::Pins::new(
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    // Set the pins to their default state
+    let pins = hal::gpio::Pins::new(
         pac.IO_BANK0,
         pac.PADS_BANK0,
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
 
-    // This is the correct pin on the Raspberry Pico board. On other boards, even if they have an
-    // on-board LED, it might need to be changed.
-    //
-    // Notably, on the Pico W, the LED is not connected to any of the RP2040 GPIOs but to the cyw43 module instead.
-    // One way to do that is by using [embassy](https://github.com/embassy-rs/embassy/blob/main/examples/rp/src/bin/wifi_blinky.rs)
-    //
-    // If you have a Pico W and want to toggle a LED with a simple GPIO output pin, you can connect an external
-    // LED to one of the GPIO pins, and reference that pin here. Don't forget adding an appropriate resistor
-    // in series with the LED.
-    let mut led_pin = pins.led.into_push_pull_output();
+    // hal::uart::UartPeripheral::new(pac.)
+
+    let uart0 = hal::uart::UartPeripheral::new(
+        pac.UART0,
+        (pins.gpio12.into_function(), pins.gpio13.into_function()),
+        &mut pac.RESETS,
+    )
+    .enable(
+        UartConfig::new(19200.Hz(), DataBits::Eight, None, StopBits::One),
+        clocks.peripheral_clock.freq(),
+    )
+    .unwrap();
+
+    let uart1 = hal::uart::UartPeripheral::new(
+        pac.UART1,
+        (pins.gpio8.into_function(), pins.gpio9.into_function()),
+        &mut pac.RESETS,
+    )
+    .enable(
+        UartConfig::new(19200.Hz(), DataBits::Eight, None, StopBits::One),
+        clocks.peripheral_clock.freq(),
+    )
+    .unwrap();
+
+    let mut raw_buf0: [u8; 512] = [0; 512];
+    let mut raw_buf1: [u8; 512] = [0; 512];
+    let mut read_buf0: [u8; 512] = [0; 512];
+    let mut read_buf1: [u8; 512] = [0; 512];
+    let mut pos0: usize = 0;
+    let mut pos1: usize = 0;
 
     loop {
-        info!("on!");
-        led_pin.set_high().unwrap();
-        delay.delay_ms(500);
-        info!("off!");
-        led_pin.set_low().unwrap();
-        delay.delay_ms(500);
+        // uart0
+        //     .read_full_blocking(&mut raw_buf0)
+        //     .map_err(|e| {
+        //         let e_str = match e {
+        //             ReadErrorType::Break => "break",
+        //             ReadErrorType::Framing => "framing",
+        //             ReadErrorType::Overrun => "overrun",
+        //             ReadErrorType::Parity => "parity",
+        //         };
+        //         error!("uart0 {}", e_str);
+        //         None::<&str>
+        //     })
+        //     .expect("should not fail");
+
+        uart1
+            .read_full_blocking(&mut raw_buf1)
+            .map_err(|e| {
+                let e_str = match e {
+                    ReadErrorType::Break => "break",
+                    ReadErrorType::Framing => "framing",
+                    ReadErrorType::Overrun => "overrun",
+                    ReadErrorType::Parity => "parity",
+                };
+                error!("uart1 {}", e_str);
+                None::<&str>
+            })
+            .expect("should not fail");
+
+        // match raw_buf0[0] {
+        //     b'\n' => {
+        //         println!("{}", str::from_utf8(&read_buf0).unwrap());
+        //         pos0 = 0;
+        //     }
+        //     c => {
+        //         read_buf0[pos0] = c;
+        //         pos0 += 1;
+        //     }
+        // }
+
+        // match raw_buf1[0] {
+        //     b'\n' => {
+        //         println!("{}", str::from_utf8(&read_buf1).unwrap());
+        //         pos1 = 0;
+        //     }
+        //     c => {
+        //         read_buf1[pos1] = c;
+        //         pos1 += 1;
+        //     }
+        // }
+
+        // println!("buf0: {}", str::from_utf8(&raw_buf0).unwrap());
+        println!("buf1: {}", str::from_utf8(&raw_buf1).unwrap());
     }
 }
 
