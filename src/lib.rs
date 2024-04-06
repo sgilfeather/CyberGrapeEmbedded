354d83e558a18f23b070070007462c1d2a06a838
liam-strand <Liam.Strand@tufts.edu>
2024-04-06 00:39:22 -0400

    documentation, lots of cleanup, and newer hal+bsp

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,27 @@
 #![no_std]
 
+// Okay this is fucky so get ready.
+
+// This is the API that talc expects for our lock
 use lock_api::{GuardSend, RawMutex};
+
+// This is how we access the real hardware locks on the pico
 use rp_pico::hal::sio::{Spinlock, SpinlockValid};
 
+// We need to write an adapter that uses the SpinLock to implement the RawMutex
+// API, so that the hardware locks can be used in our custom allocator.
+
+// This struct essentially holds a marker telling the compiler that it exists,
+// but it doesn't have any real data in it, this will be our adapter.
+// The const type parameter N indicate which hardware lock we are referring to.
 pub struct HardwareSpinlock<const N: usize>(core::marker::PhantomData<()>);
 
+// This is obviously wildly unsafe because we, the programmer, are promising
+// that the locks will actually block when they are supposed to. It would be
+// trivial to write a RawMutex implementation that does not actually lock.
 unsafe impl<const N: usize> RawMutex for HardwareSpinlock<N>
 where
+    // This enforces that N refers to a real hardware lock
     Spinlock<N>: SpinlockValid,
 {
     type GuardMarker = GuardSend;
@@ -20,6 +35,9 @@ where
         Spinlock::<N>::try_claim().is_some()
     }
 
+    // RawMutex::unlock is unsafe to call because it must be called in the same
+    // scope as RawMutex::lock was called. We normally release locks by letting
+    // them go out of scope.
     unsafe fn unlock(&self) {
         Spinlock::<N>::release();
     }
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
-//! # UART Example
-//!
-//! This application demonstrates how to use the UART Driver to talk to a serial
-//! connection.
-//!
-//! It may need to be adapted to your particular board layout and/or pin
-//! assignment.
-//!
-//! See the `Cargo.toml` file for Copyright and license details.
+//! TODO
 
 #![no_std]
 #![no_main]
 
-use alloc::format;
 // Ensure we halt the program on panic (if we don't mention this crate it won't
 // be linked)
 use panic_halt as _;
 
 // Alias for our HAL crate
-use rp_pico::entry;
 use rp_pico::hal;
 
 // A shorter alias for the Peripheral Access Crate, which provides low-level
 // register access
 use hal::pac;
 
+#[allow(unused)]
 use defmt::*;
 use defmt_rtt as _;
 use nb::Error;
@@ -35,31 +26,30 @@ use hal::fugit::RateExtU32;
 // UART related types
 use hal::uart::{DataBits, StopBits, UartConfig};
 
-extern crate alloc;
+// All of the things we need to get the heap allocator working
 use cg::HardwareSpinlock;
-use talc::*;
+use rp_pico::hal::uart::{ReadError, Reader, UartDevice, ValidUartPinout, Writer};
+use talc::{ClaimOnOom, Span, Talc, Talck};
 
+extern crate alloc;
 use alloc::vec::Vec;
 
+// Some stack space to store our heap
 static mut ARENA: [u8; 16384] = [0; 16384];
 
+// Instantiate an allocator, give it the arena, and tell Rust that we want this
+// to be our global allocator for all dynamically-sized types.
+// Those dynamically sized types need to come from alloc, not std.
 #[global_allocator]
 static ALLOCATOR: Talck<HardwareSpinlock<13>, ClaimOnOom> =
     Talc::new(unsafe { ClaimOnOom::new(Span::from_const_array(core::ptr::addr_of!(ARENA))) })
         .lock();
 
-/// External high-speed crystal on the Raspberry Pi Pico board is 12 MHz. Adjust
-/// if your board has a different frequency
-const XTAL_FREQ_HZ: u32 = 12_000_000u32;
-
 /// Entry point to our bare-metal application.
 ///
-/// The `#[rp2040_hal::entry]` macro ensures the Cortex-M start-up code calls this function
+/// The `#[rp_pico::entry]` macro ensures the Cortex-M start-up code calls this function
 /// as soon as all global variables and the spinlock are initialised.
-///
-/// The function configures the RP2040 peripherals, then writes to the UART in
-/// an infinite loop.
-#[entry]
+#[rp_pico::entry]
 fn main() -> ! {
     // Grab our singleton objects
     let mut pac = pac::Peripherals::take().unwrap();
@@ -70,7 +60,7 @@ fn main() -> ! {
 
     // Configure the clocks
     let clocks = hal::clocks::init_clocks_and_plls(
-        XTAL_FREQ_HZ,
+        rp_pico::XOSC_CRYSTAL_FREQ,
         pac.XOSC,
         pac.CLOCKS,
         pac.PLL_SYS,
@@ -78,8 +68,7 @@ fn main() -> ! {
         &mut pac.RESETS,
         &mut watchdog,
     )
-    .map_err(|_| "could not get it to work")
-    .unwrap();
+    .expect("Should be able to initialize clocks");
 
     let mut _delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
@@ -93,9 +82,10 @@ fn main() -> ! {
         sio.gpio_bank0,
         &mut pac.RESETS,
     );
-    // hal::uart::UartPeripheral::new(pac.)
 
-    let uart0 = hal::uart::UartPeripheral::new(
+    // Instantiate our two UART channels, then split them so that we have a
+    // two readers and a writer (we throw away the uart0 writer)
+    let (uart0, _) = hal::uart::UartPeripheral::new(
         pac.UART0,
         (pins.gpio12.into_function(), pins.gpio13.into_function()),
         &mut pac.RESETS,
@@ -104,7 +94,8 @@ fn main() -> ! {
         UartConfig::new(115200.Hz(), DataBits::Eight, None, StopBits::One),
         clocks.peripheral_clock.freq(),
     )
-    .unwrap();
+    .unwrap()
+    .split();
 
     let (uart1, writer) = hal::uart::UartPeripheral::new(
         pac.UART1,
@@ -118,55 +109,57 @@ fn main() -> ! {
     .unwrap()
     .split();
 
+    // These bad boys are going to hold the raw data we get out of the UART
+    // channel
     let mut raw_buf0: [u8; 512] = [0; 512];
     let mut raw_buf1: [u8; 512] = [0; 512];
+
+    // Then these ones will be where we assemble the messages
     let mut read_buf0: Vec<u8> = Vec::new();
     let mut read_buf1: Vec<u8> = Vec::new();
 
     loop {
-        let read0 = uart0
-            .read_raw(&mut raw_buf0)
-            .unwrap_or_else(|err| match err {
-                Error::WouldBlock => 0,
-                Error::Other(err) => {
-                    error!("FAIL {}", format!("{:?}", err).as_str());
-                    core::panic!();
-                }
-            });
-        for i in 0..read0 {
-            match raw_buf0[i] {
-                b'\n' => {
-                    read_buf0.push(b'\n');
-                    let rest = writer.write_raw(&read_buf0).unwrap();
-                    read_buf0.clear();
-                    // read_buf0.extend(rest);
-                }
-                c => {
-                    read_buf0.push(c);
-                }
-            }
-        }
+        let read0 = perform_read(&mut raw_buf0, &uart0).expect("Read from UART0 should succeed");
+        handle_read(read0, &raw_buf0, &mut read_buf0, &writer);
 
-        let read1 = uart1
-            .read_raw(&mut raw_buf1)
-            .unwrap_or_else(|err| match err {
-                Error::WouldBlock => 0,
-                Error::Other(err) => {
-                    error!("FAIL {}", format!("{:?}", err).as_str());
-                    core::panic!();
-                }
-            });
-        for i in 0..read1 {
-            match raw_buf1[i] {
-                b'\n' => {
-                    read_buf1.push(b'\n');
-                    writer.write_raw(&read_buf1);
-                    read_buf1.clear();
-                }
-                c => {
-                    read_buf1.push(c);
-                }
-            }
+        let read1 = perform_read(&mut raw_buf1, &uart1).expect("Read from UART1 should succeed");
+        handle_read(read1, &raw_buf1, &mut read_buf1, &writer);
+    }
+}
+
+/// Attempts to read from the UART Reader, but instead of having a special error
+/// case when the read queue is empty, simply returns 0 to indicate that no
+/// characters have been read. Any other read errors are propigated.
+fn perform_read<'a, D, P>(
+    raw_buf: &'a mut [u8],
+    reader: &Reader<D, P>,
+) -> Result<usize, ReadError<'a>>
+where
+    D: UartDevice,
+    P: ValidUartPinout<D>,
+{
+    match reader.read_raw(raw_buf) {
+        Ok(len) => Ok(len),
+        Err(Error::WouldBlock) => Ok(0),
+        Err(Error::Other(other)) => Err(other),
+    }
+}
+
+/// Handles processing a read from the UART, appending to the read buffer and
+/// flushing that buffer out to the writer if it sees a newline character
+fn handle_read<D, P>(read_len: usize, raw_buf: &[u8], read_buf: &mut Vec<u8>, writer: &Writer<D, P>)
+where
+    D: UartDevice,
+    P: ValidUartPinout<D>,
+{
+    for &c in raw_buf.iter().take(read_len) {
+        read_buf.push(c);
+
+        if c == b'\n' {
+            let _rest = writer
+                .write_raw(read_buf)
+                .expect("Write to UART1 should succeed");
+            read_buf.clear();
         }
     }
 }
